d507793230aabce4f776d33c668a923fd59134f3
Mister-Axolotl <krlekraft@gmail.com>
2025-10-06 20:51:36 +0200

    Update sample data and enhance main page layout

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,8 @@
-INSERT INTO users (username, email, password) VALUES ('Florent', 'florent@example.com', 'azerty');
 INSERT INTO users (username, email, password) VALUES ('Grégory', 'gregory@example.com', 'azerty');
-INSERT INTO tasks (title, description, done, user_id) VALUES ('Première tâche', 'Exemple', false, 1);
+INSERT INTO users (username, email, password) VALUES ('Florent', 'florent@example.com', 'azerty');
+INSERT INTO tasks (title, description, done, user_id) VALUES ('Manger du chocolat', 'Miam miam', false, 1);
+INSERT INTO tasks (title, description, done, user_id) VALUES ('Terminer le devoir', 'Java', true, 1);
+INSERT INTO tasks (title, description, done, user_id) VALUES ('Dormir', 'Zzzzzzzz', false, 1);
+INSERT INTO tasks (title, description, done, user_id) VALUES ('Mettre une bonne note à Grégory', 'Environ 15/20', true, 2);
+INSERT INTO tasks (title, description, done, user_id) VALUES ('Entrer les notes sur Classroom', 'Ne pas oublier quelqu''un', false, 2);
+INSERT INTO tasks (title, description, done, user_id) VALUES ('Prendre une pause bien méritée', 'Se détendre devant un bon film', false, 2);
